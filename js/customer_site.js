96db94066afe03299a66c67b7616859da012c45b
Jobs-Devin <30886537+zhangzeyu007@users.noreply.github.com>
2025-08-17 21:04:47 +0800

    Update customer_site.js
    接口bug修复

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,27 +2,27 @@ const CUSTOMER_SITES = {
    "dyttzy": {
          "api": "http://caiji.dyttzyapi.com/api.php/provide/vod",
          "name": "电影天堂资源",
-         "detail": "http://caiji.dyttzyapi.com/api.php/provide/vod"
+         "url": "http://caiji.dyttzyapi.com/api.php/provide/vod"
        },
    "pingguo": {
          "api": "https://ikunzyapi.com/api.php/provide/vod",
          "name": "苹果",
-         "detail": "https://ikunzyapi.com/api.php/provide/vod"
+         "url": "https://ikunzyapi.com/api.php/provide/vod"
        },
     "lzi": {
          "api": "https://cj.lziapi.com/api.php/provide/vod",
          "name": "量子",
-         "detail": "https://cj.lziapi.com/api.php/provide/vod"
+         "url": "https://cj.lziapi.com/api.php/provide/vod"
        },
     "ruyi": {
          "api": "https://cj.rycjapi.com/api.php/provide/vod",
          "name": "如意",
-         "detail": "https://cj.rycjapi.com/api.php/provide/vod"
+         "url": "https://cj.rycjapi.com/api.php/provide/vod"
        },
     "baofeng": {
          "api": "https://bfzyapi.com/api.php/provide/vod",
          "name": "暴风",
-         "detail": "https://bfzyapi.com/api.php/provide/vod"
+         "url": "https://bfzyapi.com/api.php/provide/vod"
        } 
 };
 
